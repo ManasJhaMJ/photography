4f7b03a941bec544ba27c05287cf96b77c3df651
manasjhamj <jhamanas939@gmail.com>
2024-04-26 18:25:30 +0530

    add  Splide carousel and other sections

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Change navbar properties on scroll
 var navbar = document.querySelector('nav')
 window.onscroll = function () {
-    if (window.scrollY > 500) {
+    if (window.scrollY > 300) {
         navbar.classList.add('scrolled')
     } else {
         navbar.classList.remove('scrolled')
@@ -25,4 +25,16 @@ window.onclick = function (event) {
             }
         }
     }
-}  
+}
+
+// Splide
+var splide = new Splide('.splide', {
+    type: 'loop',
+    height: '50vh',
+    focus: 'center',
+    autoplay: true,
+    interval: 1500,
+    autoWidth: true,
+});
+
+splide.mount(window.splide.Extensions);
